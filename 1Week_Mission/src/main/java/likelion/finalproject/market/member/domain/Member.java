8a47e37b919f11ca200e426a139a60c53d4bc246
devmizz <garamminchan@gmail.com>
2022-10-17 18:16:23 +0900

    feat: save auth as string on entity

--- 1Week_Mission/src/main/java/likelion/finalproject/market/member/domain/Member.java
+++ 1Week_Mission/src/main/java/likelion/finalproject/market/member/domain/Member.java
@@ -6,12 +6,16 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @NoArgsConstructor
 @Getter
+//@Table(name="MEMBERS")
 @Entity
 public class Member {
 
@@ -33,6 +37,7 @@ public class Member {
     private String password;
 
     @Column
+    @Enumerated(EnumType.STRING)
     private Auth auth;
 
     @Builder

3eb560e186c6df704e8fd5135bf3152d86fa2e06
devmizz <garamminchan@gmail.com>
2022-10-17 17:32:23 +0900

    feat: member join view

--- 1Week_Mission/src/main/java/likelion/finalproject/market/member/api/MemberJoinApi.java
+++ 1Week_Mission/src/main/java/likelion/finalproject/market/member/api/MemberJoinApi.java
@@ -1,26 +1,26 @@
 package likelion.finalproject.market.member.api;
 
 import likelion.finalproject.market.member.application.MemberJoinService;
-        import likelion.finalproject.market.member.dto.request.RequestJoin;
-        import likelion.finalproject.market.member.dto.response.ResponseMember;
-        import lombok.RequiredArgsConstructor;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.PostMapping;
-        import org.springframework.web.bind.annotation.RequestBody;
-        import org.springframework.web.bind.annotation.RestController;
+import likelion.finalproject.market.member.dto.request.RequestJoin;
+import likelion.finalproject.market.member.dto.response.ResponseMember;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class MemberJoinApi {
     private final MemberJoinService memberJoinService;
 
-    @GetMapping("member/join")
+    @GetMapping("/member/join")
     public String join() {
-        return "join";
+        return "/member/join";
     }
 
-    @PostMapping("member/join")
+    @PostMapping("/member/join")
     public ResponseEntity<ResponseMember> join (
             @RequestBody RequestJoin requestJoin
     ) {

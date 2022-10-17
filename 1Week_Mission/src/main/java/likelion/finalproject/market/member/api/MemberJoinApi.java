3d1fce159e5c692b2829b64b8f3cbfcbe5da37fb
devmizz <garamminchan@gmail.com>
2022-10-17 18:12:56 +0900

    feat: view transfer object to controller

--- 1Week_Mission/src/main/java/likelion/finalproject/market/member/api/MemberJoinApi.java
+++ 1Week_Mission/src/main/java/likelion/finalproject/market/member/api/MemberJoinApi.java
@@ -6,7 +6,11 @@ import likelion.finalproject.market.member.dto.response.ResponseMember;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -16,13 +20,14 @@ public class MemberJoinApi {
     private final MemberJoinService memberJoinService;
 
     @GetMapping("/member/join")
-    public String join() {
+    public String join(Model model) {
+        model.addAttribute("requestJoin", new RequestJoin());
         return "/member/join";
     }
 
     @PostMapping("/member/join")
     public ResponseEntity<ResponseMember> join (
-            @RequestBody RequestJoin requestJoin
+            @ModelAttribute("requestJoin") RequestJoin requestJoin
     ) {
         ResponseMember responseMember = memberJoinService.register(requestJoin);
         return ResponseEntity.ok(responseMember);
